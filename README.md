805a4f896edfc3c0e7b3988e44e9ce28244ffd06
uvsqNathanAmoussou <nathan.amoussou@ens.uvsq.fr>
2024-05-01 13:33:41 +0200

    lotta stuffs dawg

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # RAM_DM
 Devoir maison en IN620 - Modèle de calcul et complexité.
-À rendre le dimanche 5 mai 2024 à 23h59.
+
+Instructions RAM :
+Instructions arithmétiques (les deux opérandes sont dans r1, r2 et le résultat dans r3) :
+    ADD(r1,r2,r3)
+    SUB(r1,r2,r3)
+    MULT(r1,r2,r3)
+    DIV(r1,r2,r3)
+Instructions de contrôle :
+    JUMP(z)
+    JE(r1,r2,z)
+    JL(r1,r2,z) (si la valeur de r1 est égale (plus grande pour JL) à r2 sauter de z instructions)
--- main.py
+++ main.py
@@ -3,14 +3,45 @@ import argparse # Pour récupérer le chemin du fichier avec les instructions RA
 # Question 1
 
 # Récupération du chemin du fichier en argument.
-RAM_instructions = list() # Pour stocker chaque instruction RAM.
+RAM_instructions_list = list() # Pour stocker chaque instruction RAM.
 parser = argparse.ArgumentParser(description='Lire un fichier contenant des instructions RAM.')
 parser.add_argument("filepath", help="Chemin du fichier à traiter", type=str)
 args = parser.parse_args()
 filepath = args.filepath
 print(f"Traitement du fichier : {filepath}")
-
-# Extraction des instructions RAM.
 for line in open(filepath, 'r'):
-    RAM_instructions.append(line.strip())
-print(RAM_instructions)
+    RAM_instructions_list.append(line.strip())
+
+# Création de la classe pour les instructions RAM.
+class RAM_instruction:
+    def __init__(self, instruction: str):
+        self.str_instruc = instruction # Instruction sous la forme 'ADD(r1,r2,r3)'
+        self.instruc_elements = self.parse() # Instruction sous la forme ('ADD', ['r1', 'r2', 'r3'])
+        self.instruc_type = self.instruc_elements[0] # Type de l'instruction (ADD, SUB, ...)
+        self.instruc_args = self.instruc_elements[1] # Arguments de l'instruction (['r1', 'r2', 'r3'])
+        
+    def parse(self):
+        # On récupère l'instruction et les arguments.
+        instruction, arguments = self.str_instruc.split("(")
+        arguments = arguments[:-1]
+        arguments = arguments.split(",")
+        return instruction, arguments
+
+# Création de la classe pour le programme RAM.
+class RAM_program:
+    def __init__(self, RAM_instructions_list_arg, PC: int = 0):
+        self.RAM_instructions_list = [RAM_instruction(RAM_instruction_arg) for RAM_instruction_arg in RAM_instructions_list_arg]
+        self.PC = PC
+    
+    def RAM_program_print(self):
+        print("Flow d'instructions RAM :")
+        for i in range(len(self.RAM_instructions_list)):
+            print(f"{i} : {self.RAM_instructions_list[i].str_instruc}")
+        print("\nDétails des instructions RAM :")
+        for instruc in self.RAM_instructions_list:
+            print(f"Instruction : {instruc.str_instruc}")
+            print(f"Type : {instruc.instruc_type}")
+            print(f"Arguments : {instruc.instruc_args}")
+
+RAM_program1 = RAM_program(RAM_instructions_list)
+RAM_program1.RAM_program_print()
